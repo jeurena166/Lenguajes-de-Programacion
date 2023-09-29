1d4987172d2a12935220d4b9518b5e9287169260
Jean Erick Ureña <112443525+jeurena166@users.noreply.github.com>
2023-09-28 23:33:22 -0600

    Create ejercicio5.pl

---
+++ Practica Prolog/ejercicio5.pl
@@ -0,0 +1,9 @@
+%Implemente un predicado que, a partir de una lista de cadenas string, filtre aquellas que contengan una subcadena que el usuario indique en otro argumento. 
+
+sub_cadenas(_, [], []).
+sub_cadenas(Subcadena, [Cadena|CadenasRestantes], Filtradas) :-
+    sub_string(Cadena, _, _, _, Subcadena),
+    sub_cadenas(Subcadena, CadenasRestantes, FiltradasRestantes),
+    Filtradas = [Cadena|FiltradasRestantes].
+sub_cadenas(Subcadena, [_|CadenasRestantes], Filtradas) :-
+    sub_cadenas(Subcadena, CadenasRestantes, Filtradas).
